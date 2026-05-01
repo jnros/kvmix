805240900c3359264812c2145d7f6d395895bb02
John Rose <119077756+jnros@users.noreply.github.com>
2026-05-01 14:56:48 -0500

    refactor for 3 strats

--- quant.c
+++ quant.c
@@ -49,112 +49,128 @@ static void print_report(int head, float max, float total_mse)
 		printf("%s strat %d head %d max %0.6f MSE %0.6f\n", fn, strat, head, max, total_mse/(TOKENS*DIM));
 }
 
-static int strat1(tensor_t *tensor)
+static double quant_head_per_tensor(tensor_t *tensor, int head, float *maxout)
 {
-	float scale_h0;
-	float scale_h1;
-	float v0;
-	float v1;
-	float tok_max_h0 = 0.0;
-	float tok_max_h1 = 0.0;
-	double total_mse0 = 0.0;
-	double total_mse1 = 0.0;
-	float orig_h0, orig_h1;
-	int8_t q_h0, q_h1;
-	float deq_h0, deq_h1;
+	float max = 0.0, scale, orig, deq, v;
+	double mse = 0.0;
+	int8_t q;
+	int i, j;
+
+	for (i = 0; i < TOKENS; i++)
+		for (j = 0; j < DIM; j++) {
+			v = fabsf((float) tensor[i].head[head].values[j]);
+			if (v > max)
+				max = v;
+		}
+
+	scale = 127 / max;
+	for (i = 0; i < TOKENS; i++)
+		for (j = 0; j < DIM; j++) {
+			orig = (float) tensor[i].head[head].values[j];
+			q = (int8_t) nearbyintf(scale * orig);
+			deq = (float) q / scale;
+			mse = mse + ((deq - orig)*(deq - orig));
+		}
+
+	*maxout = max;
+	return mse;
+}
+
+static double quant_head_per_token(tensor_t *tensor, int head, float *maxout)
+{
+	float max = 0.0, tok_max, scale, orig, deq, v;
+	double mse = 0.0;
+	int8_t q;
 	int i, j;
 
 	for (i = 0; i < TOKENS; i++) {
+		tok_max = 0;
 		for (j = 0; j < DIM; j++) {
-			v0 = fabsf((float) tensor[i].head[0].values[j]);
-			v1 = fabsf((float) tensor[i].head[1].values[j]);		
-			if (v0 > tok_max_h0) 
-				tok_max_h0 = v0;
-			if (v1 > tok_max_h1) 
-				tok_max_h1 = v1;
+			v = fabsf((float) tensor[i].head[head].values[j]);
+			if (v > tok_max)
+				tok_max = v;
 		}
+		scale = 127 / tok_max;
+		for (j = 0; j < DIM; j++) {
+			orig = (float) tensor[i].head[head].values[j];
+			q = (int8_t) nearbyintf(scale * orig);
+			deq = (float) q / scale;
+			mse = mse + ((deq - orig)*(deq - orig));
+		}
+		if (tok_max > max)
+			max = tok_max;
 	}
 
-	scale_h0 = 127 / tok_max_h0;
-	scale_h1 = 127 / tok_max_h1;
+	*maxout = max;
+	return mse;
+}
 
-	for (i = 0; i < TOKENS; i++) {
-		for (j = 0; j < DIM; j++) {
-			orig_h0 = (float) tensor[i].head[0].values[j];
-			orig_h1 = (float) tensor[i].head[1].values[j];
+static int strat1(tensor_t *tensor)
+{
+	float max0, max1;
+	double mse0, mse1;
 
-			q_h0 = (int8_t) nearbyintf(scale_h0 * orig_h0);
-			q_h1 = (int8_t) nearbyintf(scale_h1 * orig_h1);
-
-			deq_h0 = (float) q_h0 / scale_h0;
-			deq_h1 = (float) q_h1 / scale_h1;
-
-			total_mse0 = total_mse0 + ((deq_h0 - orig_h0)*(deq_h0 - orig_h0));
-			total_mse1 = total_mse1 + ((deq_h1 - orig_h1)*(deq_h1 - orig_h1));
-		}
-	}
-
-	print_report(0, tok_max_h0, total_mse0);
-	print_report(1, tok_max_h1, total_mse1);
-
+	mse0 = quant_head_per_tensor(tensor, 0, &max0);
+	mse1 = quant_head_per_tensor(tensor, 1, &max1);
+	print_report(0, max0, mse0);
+	print_report(1, max1, mse1);
 	return 0;
 }
 
 static int strat2(tensor_t *tensor)
 {
-	float scale_h0;
-	float scale_h1;
-	float v0;
-	float v1;
-	float tok_max_h0 = 0.0;
-	float tok_max_h1 = 0.0;
-	float max_h0 = 0.0;
-	float max_h1 = 0.0;
-	double total_mse0 = 0.0;
-	double total_mse1 = 0.0;
-	float orig_h0, orig_h1;
-	int8_t q_h0, q_h1;
-	float deq_h0, deq_h1;
-	int i, j;
+	float max0, max1;
+	double mse0, mse1;
 
-	for (i = 0; i < TOKENS; i++) {
-		tok_max_h0 = 0;
-		tok_max_h1 = 0;
-		for (j = 0; j < DIM; j++) {
-			v0 = fabsf((float) tensor[i].head[0].values[j]);
-			v1 = fabsf((float) tensor[i].head[1].values[j]);		
-			if (v0 > tok_max_h0)
-				tok_max_h0 = v0;
-			if (v1 > tok_max_h1)
-				tok_max_h1 = v1;
-		}
+	mse0 = quant_head_per_token(tensor, 0, &max0);
+	mse1 = quant_head_per_token(tensor, 1, &max1);
+	print_report(0, max0, mse0);
+	print_report(1, max1, mse1);
+	return 0;
+}
 
-		scale_h0 = 127 / tok_max_h0;
-		scale_h1 = 127 / tok_max_h1;
+static int k_outlier(int layer, int head)
+{
+	static const int ol[][2] = {
+		{0,0},{0,1},{1,1},{2,0},{2,1},{8,0}
+	};
+	int i;
+	for (i = 0; i < 6; i++)
+		if (ol[i][0] == layer && ol[i][1] == head)
+			return 1;
+	return 0;
+}
 
-		for (j = 0; j < DIM; j++) {
-			orig_h0 = (float) tensor[i].head[0].values[j];
-			orig_h1 = (float) tensor[i].head[1].values[j];
+static int strat3(tensor_t *tensor)
+{
+	char *fn = strrchr(in_filename, '/');
+	int layer, rc, h;
+	char korv;
+	float max;
+	double mse;
 
-			q_h0 = (int8_t) nearbyintf(scale_h0 * orig_h0);
-			q_h1 = (int8_t) nearbyintf(scale_h1 * orig_h1);
+	if (fn)
+		fn = fn + 1;
+	else
+		fn = in_filename;
 
-			deq_h0 = (float) q_h0 / scale_h0;
-			deq_h1 = (float) q_h1 / scale_h1;
-			
-			total_mse0 = total_mse0 + ((deq_h0 - orig_h0)*(deq_h0 - orig_h0));
-			total_mse1 = total_mse1 + ((deq_h1 - orig_h1)*(deq_h1 - orig_h1));
-		}
-		if (tok_max_h0 > max_h0)
-			max_h0 = tok_max_h0;
-		
-		if (tok_max_h1 > max_h1)
-			max_h1 = tok_max_h1;
+	rc = sscanf(fn, "layer_%d.%c.bin", &layer, &korv);
+	if (rc != 2) {
+		fprintf(stderr, "error: unexpected filename\n");
+		return 1;
 	}
 
-	print_report(0, max_h0, total_mse0);
-	print_report(1, max_h1, total_mse1);
-
+	for (h = 0; h < HEADS; h++) {
+		if (korv == 'k' && k_outlier(layer, h)) {
+			print_report(h, 0.0, 0.0);
+		} else if (korv == 'v') {
+			mse = quant_head_per_token(tensor, h, &max);
+			print_report(h, max, mse);
+		} else {
+			mse = quant_head_per_tensor(tensor, h, &max);
+			print_report(h, max, mse);
+		}
+	}
 	return 0;
 }
 
@@ -187,6 +203,8 @@ int mainloop()
 		strat1(tensor);
 	else if (strat == 2)
 		strat2(tensor);
+	else if (strat == 3)
+		strat3(tensor);
 
 	rc = munmap(tensor, st.st_size);
 	if (rc == -1) {
